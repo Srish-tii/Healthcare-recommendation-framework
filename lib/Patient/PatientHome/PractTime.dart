ef033c69d03cbe3867f1ea561f5df0abe0ee82a8
Srishti Lodha <srishtilodha2000@gmail.com>
2023-08-10 12:54:37 +0530

    [FEAT] Activity and date selection

--- lib/Patient/PatientHome/PractTime.dart
+++ lib/Patient/PatientHome/PractTime.dart
@@ -1,20 +1,26 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:rastreador/Patient/PatientHome/AddAct.dart';
 import 'package:syncfusion_flutter_datepicker/datepicker.dart';
 import 'ListActivities.dart';
 
-class PractTime extends StatelessWidget {
+class PractTime extends StatefulWidget {
   final LocationData? selectedActivity;
 
   PractTime({this.selectedActivity});
+
+  @override
+  _PractTimeState createState() => _PractTimeState();
+}
+
+class _PractTimeState extends State<PractTime> {
+  DateTime? _selectedDate;
 
   DateRangePickerController _datePickerController = DateRangePickerController();
 
   @override
   Widget build(BuildContext context) {
     print(
-        'Selected Activity: ID - ${selectedActivity?.id}, Address - ${selectedActivity?.address}');
+        'Selected Activity: ID - ${widget.selectedActivity?.id}, Address - ${widget.selectedActivity?.address}');
     return SafeArea(
       child: Scaffold(
         appBar: AppBar(
@@ -22,21 +28,46 @@ class PractTime extends StatelessWidget {
         ),
         body: SfDateRangePicker(
           view: DateRangePickerView.month,
-          selectionMode: DateRangePickerSelectionMode.multiRange,
+          selectionMode:
+              DateRangePickerSelectionMode.single, // Change selection mode
           showActionButtons: true,
           controller: _datePickerController,
           onSubmit: (Object? val) {
-            print(val);
-            Navigator.push(
-              context,
-              MaterialPageRoute(builder: (context) => LActivities()),
-            );
+            if (val is DateTime) {
+              setState(() {
+                _selectedDate = val;
+              });
+              _showConfirmationDialog();
+            }
           },
           onCancel: () {
-            _datePickerController.selectedRanges = null;
+            _datePickerController.selectedDate = null;
           },
         ),
       ),
+    );
+  }
+
+  void _showConfirmationDialog() {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text('Scheduling Activity'),
+          content: Text(
+            'Scheduling activity ID ${widget.selectedActivity?.id} at ${widget.selectedActivity?.address} on ${_selectedDate?.day}/${_selectedDate?.month}/${_selectedDate?.year}',
+          ),
+          actions: <Widget>[
+            TextButton(
+              onPressed: () {
+                Navigator.pop(context); // Close the pop-up
+                Navigator.pop(context); // Go back to the previous screen
+              },
+              child: Text('OK'),
+            ),
+          ],
+        );
+      },
     );
   }
 }
