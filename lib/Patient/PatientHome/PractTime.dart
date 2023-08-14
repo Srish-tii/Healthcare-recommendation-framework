1625fd067ff3540ec7d81b039ba585fe26425ef6
Srishti Lodha <srishtilodha2000@gmail.com>
2023-08-14 16:31:02 +0530

    added more scheduling options

--- lib/Patient/PatientHome/PractTime.dart
+++ lib/Patient/PatientHome/PractTime.dart
@@ -1,81 +1,8 @@
-// import 'package:flutter/material.dart';
-// import 'package:rastreador/Patient/PatientHome/AddAct.dart';
-// import 'package:syncfusion_flutter_datepicker/datepicker.dart';
-
-// class PractTime extends StatefulWidget {
-//   final LocationData? selectedActivity;
-
-//   PractTime({this.selectedActivity});
-
-//   @override
-//   _PractTimeState createState() => _PractTimeState();
-// }
-
-// class _PractTimeState extends State<PractTime> {
-//   DateTime? _selectedDate;
-
-//   DateRangePickerController _datePickerController = DateRangePickerController();
-
-//   @override
-//   Widget build(BuildContext context) {
-//     print(
-//         'Selected Activity: ID - ${widget.selectedActivity?.id}, Address - ${widget.selectedActivity?.address}');
-//     return SafeArea(
-//       child: Scaffold(
-//         appBar: AppBar(
-//           title: Text('Select Time of Practicing'),
-//         ),
-//         body: SfDateRangePicker(
-//           view: DateRangePickerView.month,
-//           selectionMode:
-//               DateRangePickerSelectionMode.single, // Change selection mode
-//           showActionButtons: true,
-//           controller: _datePickerController,
-//           onSubmit: (Object? val) {
-//             if (val is DateTime) {
-//               setState(() {
-//                 _selectedDate = val;
-//               });
-//               _showConfirmationDialog();
-//             }
-//           },
-//           onCancel: () {
-//             _datePickerController.selectedDate = null;
-//           },
-//         ),
-//       ),
-//     );
-//   }
-
-//   void _showConfirmationDialog() {
-//     showDialog(
-//       context: context,
-//       builder: (BuildContext context) {
-//         return AlertDialog(
-//           title: Text('Scheduling Activity'),
-//           content: Text(
-//             'Scheduling activity ID ${widget.selectedActivity?.id} at ${widget.selectedActivity?.address} on ${_selectedDate?.day}/${_selectedDate?.month}/${_selectedDate?.year}',
-//           ),
-//           actions: <Widget>[
-//             TextButton(
-//               onPressed: () {
-//                 Navigator.pop(context); // Close the pop-up
-//                 Navigator.pop(context); // Go back to the previous screen
-//               },
-//               child: Text('OK'),
-//             ),
-//           ],
-//         );
-//       },
-//     );
-//   }
-// }
-
 import 'package:flutter/material.dart';
 import 'package:rastreador/Patient/PatientHome/AddAct.dart';
 import 'package:syncfusion_flutter_datepicker/datepicker.dart';
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package:cloud_firestore/cloud_firestore.dart'; // Import the Firestore package
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'PatientHome.dart';
 
 FirebaseAuth _auth = FirebaseAuth.instance;
@@ -112,10 +39,31 @@ class _PractTimeState extends State<PractTime> {
           controller: _datePickerController,
           onSubmit: (Object? val) {
             if (val is DateTime) {
-              setState(() {
-                _selectedDate = val;
-              });
-              _showConfirmationDialog();
+              // Check if selected date is in the past
+              if (val.isBefore(DateTime.now())) {
+                showDialog(
+                  context: context,
+                  builder: (BuildContext context) {
+                    return AlertDialog(
+                      title: Text('Invalid Date'),
+                      content: Text('Please select a future date.'),
+                      actions: <Widget>[
+                        TextButton(
+                          onPressed: () {
+                            Navigator.pop(context);
+                          },
+                          child: Text('OK'),
+                        ),
+                      ],
+                    );
+                  },
+                );
+              } else {
+                setState(() {
+                  _selectedDate = val;
+                });
+                _showConfirmationDialog();
+              }
             }
           },
           onCancel: () {
@@ -141,7 +89,6 @@ class _PractTimeState extends State<PractTime> {
                 User? user = _auth.currentUser;
                 if (user != null) {
                   String patientId = user.uid;
-
                   await _uploadToFirestore(patientId);
                 } else {
                   print('No user is currently logged in.');
@@ -152,7 +99,37 @@ class _PractTimeState extends State<PractTime> {
                   MaterialPageRoute(builder: (context) => PatientProfile()),
                 );
               },
-              child: Text('OK'),
+              child: Text('Ok and Exit'),
+            ),
+            TextButton(
+              onPressed: () async {
+                User? user = _auth.currentUser;
+                if (user != null) {
+                  String patientId = user.uid;
+                  await _uploadToFirestore(patientId);
+                } else {
+                  print('No user is currently logged in.');
+                }
+                Navigator.pop(context);
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => WriteActivity()),
+                );
+              },
+              child: Text('Schedule another activity'),
+            ),
+            TextButton(
+              onPressed: () async {
+                User? user = _auth.currentUser;
+                if (user != null) {
+                  String patientId = user.uid;
+                  await _uploadToFirestore(patientId);
+                } else {
+                  print('No user is currently logged in.');
+                }
+                Navigator.pop(context);
+              },
+              child: Text('Schedule same activity again'),
             ),
           ],
         );
@@ -166,7 +143,7 @@ class _PractTimeState extends State<PractTime> {
         'activityId': widget.selectedActivity?.id,
         'activityAddress': widget.selectedActivity?.address,
         'selectedDate': _selectedDate,
-        'patientId': patientId, // Add the patient ID
+        'patientId': patientId,
       });
       print('Data uploaded to Firestore successfully');
     } catch (e) {
